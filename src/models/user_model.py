4605114b94b1104774a2d1cee0df4aa4a5fb650c
agsaru <aghifdgamer1101@gmail.com>
2026-04-04 15:51:01 +0000

    User model added

---
+++ src/models/user_model.py
@@ -0,0 +1,15 @@
+from typing import Annotated
+from sqlmodel import SQLModel,Field
+from pydantic import EmailStr
+from enum import Enum
+class UserRole(str,Enum):
+    viewer="viewer"
+    analyst="analyst"
+    admin="admin"
+
+class User(SQLModel,table=True):
+    id: int | None =Field(default=None,primary_key=True)
+    name: str
+    email: EmailStr = Field(unique=True)
+    role: UserRole=Field(default=UserRole.viewer)
+    password: str

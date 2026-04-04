05e4f922dfd1385e6b34ed5a86d2c2d5dea9c66b
agsaru <aghifdgamer1101@gmail.com>
2026-04-04 07:08:09 +0000

    Database connected

---
+++ src/database.py
@@ -0,0 +1,9 @@
+from sqlmodel import SQLModel, create_engine, Session
+from dotenv import load_dotenv
+import os
+load_dotenv()
+DATABASE_URL=os.getenv("DATABASE_URL")
+engine=create_engine(DATABASE_URL)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
--- src/main.py
+++ src/main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
 import uvicorn
+from database import create_db_and_tables
 app=FastAPI()
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
+    print("Database connected")
+
 
 @app.get("/")
 async def home():

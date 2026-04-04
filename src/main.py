e502b28891890a4958313774ff171b10676c4932
agsaru <aghifdgamer1101@gmail.com>
2026-04-04 19:39:50 +0000

    corsmiddleware and model imports

--- src/main.py
+++ src/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
+from models.user_model import User
+from models.financial_rec_model import FinancialRecord
 from database import create_db_and_tables
 app=FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.on_event("startup")
 def on_startup():
     create_db_and_tables()

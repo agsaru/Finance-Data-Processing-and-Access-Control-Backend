709a54c119e30e8e55adbdb499854658e563a729
agsaru <aghifdgamer1101@gmail.com>
2026-04-04 15:59:29 +0000

    Financial Record Model added

---
+++ src/models/financial_rec_model.py
@@ -0,0 +1,14 @@
+from sqlmodel import SQLModel,Field
+from enum import Enum
+from datetime import datetime, timezone
+class TransationType(str,Enum):
+    income="income"
+    expense="expense"
+
+class FinancialRecord(SQLModel,table=True):
+    id: int | None =Field(default=None, primary_key=True)
+    amount:float
+    type: TransationType
+    category:str
+    date: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    user_id: int =Field(foreign_key="user.id")
